87fb61b5b47ea76181465f1293fe1a6058388afe
Azat Khaliullin <AllisonUno@yandex.ru>
2025-05-23 20:30:11 +0400

    added new def mask_account_card

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 from src.masks import get_mask_card_number, get_mask_account
 
+from src.widget import mask_account_card
+
 print(get_mask_card_number("7000 7922 8960 6361"))
 print(get_mask_account("7000792289606361"))
+
+print(mask_account_card("Счет 73654108430135874305"))
+print(mask_account_card("счёт 73654108430135874305"))
+print(mask_account_card("Visa Gold 5999414228426353"))
---
+++ src/widget.py
@@ -0,0 +1,16 @@
+from mypy.types import Union
+
+from src.masks import get_mask_account, get_mask_card_number
+
+
+# Функция теперь имеет аннотацию по типам - какой на входе и выходе
+def mask_account_card(card_or_account_number: Union[str]) -> str:
+    """Функция, принимающая на вход номер карты или счета, возвращающая маску
+    с частичным номером карты или последними 4 цифрами номера счета"""
+    if "счет" in card_or_account_number.lower() or "счёт" in card_or_account_number.lower():
+        return f"Счет {get_mask_account(card_or_account_number)}"
+    else:
+        masked_card_number = card_or_account_number.replace(
+            card_or_account_number[-16:], get_mask_card_number(card_or_account_number[-16:])
+        )
+        return masked_card_number
