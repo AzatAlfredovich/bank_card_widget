380416d9d64731f108c1cea66b2a5e6c05d960ca
Azat Khaliullin <AllisonUno@yandex.ru>
2025-06-07 13:56:16 +0400

    corrected def in widget

--- src/widget.py
+++ src/widget.py
@@ -8,9 +8,9 @@ def mask_account_card(card_or_account_number: Union[str]) -> str:
     """Функция, принимающая на вход номер карты или счета, возвращающая маску
     с частичным номером карты или последними 4 цифрами номера счета"""
     if "счет" in card_or_account_number.lower() or "счёт" in card_or_account_number.lower():
-        card_or_account_number = card_or_account_number.replace(" ", "")
-        if card_or_account_number[-20:].isdigit():
-            return f"Счет {get_mask_account(card_or_account_number[-20:])}"
+        account_number = card_or_account_number.replace(" ", "")
+        if account_number[-20:].isdigit():
+            return f"Счет {get_mask_account(account_number[-20:])}"
         else:
             return "Номер счета должен состоять только из цифр"
     else:
