5192e1366c82b3bbe6d663690b1714fa1f887595
Azat Khaliullin <AllisonUno@yandex.ru>
2025-07-04 21:14:31 +0400

    added annotations into new func and added info into README

--- src/transactions_xlsx_read.py
+++ src/transactions_xlsx_read.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 
 def reading_xlsx(file_path: str) -> list[dict]:
-    """"""
+    """Функция, переводящая Excel-файл в список словарей"""
     df = pd.read_excel(file_path)
     result = df.to_dict(orient="records")
     return result
